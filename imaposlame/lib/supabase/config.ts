e55588a47376a851b8b1a24ef6e1186b517d4ba4
HiveSite <office.hive.me@gmail.com>
2026-05-04 13:38:32 +0200

    Add Next.js imaposlame libraries

---
+++ imaposlame/lib/supabase/config.ts
@@ -0,0 +1,2 @@
+export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "https://fjfnnpgaveoonfmwztnr.supabase.co";
+export const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "sb_publishable_T9-5p7i98BdtBIa297qa3g_pnORsDvn";
